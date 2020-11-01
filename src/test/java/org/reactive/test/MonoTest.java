8e81c8468576efd42f306e0c62b2cca1d147cf10
Al Plunge <al.plunge@gmail.com>
2020-11-01 16:55:42 +0200

    Mono subscription test covered

--- src/test/java/org/reactive/test/MonoTest.java
+++ src/test/java/org/reactive/test/MonoTest.java
@@ -1,16 +1,23 @@
 package org.reactive.test;
 
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.reactivestreams.Subscription;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 @Slf4j
 public class MonoTest {
+    String name;
+
+    @BeforeEach
+    public void init() {
+        name = "Uchiha Itachi";
+    }
 
     @Test
     public void monoSubscriber() {
-        String name = "Uchiha Itachi";
         Mono<String> mono = Mono.just(name).log();
         mono.subscribe();
         log.info("-----------------------------");
@@ -20,7 +27,6 @@ public class MonoTest {
 
     @Test
     public void monoSubscriberConsumer() {
-        String name = "Uchiha Sasuke";
         Mono<String> mono = Mono.just(name).log();
         mono.subscribe(s -> log.info("Value {}", s));
         log.info("-----------------------------");
@@ -30,13 +36,30 @@ public class MonoTest {
 
     @Test
     public void monoSubscriberConsumerError() {
-        String name = "Uchiha Buraku";
         Mono<String> mono = Mono.just(name).log().map(s -> {
             throw new RuntimeException();
         });
         mono.subscribe(s -> log.info("Value {}", s), s -> log.error("Scary"));
         log.info("-----------------------------");
         StepVerifier.create(mono).expectError(RuntimeException.class).verify();
+
+    }
+
+    @Test
+    public void monoSubscriberConsumerComplete() {
+        Mono<String> mono = Mono.just(name).log().map(String::toUpperCase);
+        mono.subscribe(s -> log.info("Value {}", s), Throwable::printStackTrace, () -> log.info("FINISHED"));
+        log.info("-----------------------------");
+        StepVerifier.create(mono).expectNext(name.toUpperCase()).verifyComplete();
+
+    }
+
+    @Test
+    public void monoSubscriberConsumerSubscription() {
+        Mono<String> mono = Mono.just(name).log().map(String::toUpperCase);
+        mono.subscribe(s -> log.info("Value {}", s), Throwable::printStackTrace, () -> log.info("FINISHED"), Subscription::cancel);
+        log.info("-----------------------------");
+        StepVerifier.create(mono).expectNext(name.toUpperCase()).verifyComplete();
 
     }
 }
